44cf4831da231d58cc0c503675f7ba6efcbac4d3
Kshitiz Khanal <khanal.xitiz@gmail.com>
2023-06-06 19:06:09 +0300

    check number

---
+++ index.js
@@ -0,0 +1,13 @@
+function checknumber(n) {
+    if (n%2==0) {
+        return ("even")
+    } else if (n%2==1) {
+        return ("odd")
+    } else {
+        return ("floating number")
+    }
+
+
+    
+}
+console.log(checknumber(5))
